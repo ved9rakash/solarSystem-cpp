2d5d3e653c7a21a01f0e9d40f276b904b03d09c5
Ved9rakash <vs19990711@gmail.com>
2023-06-03 23:27:33 +0530

    Add project files.

---
+++ solarsimulation/texture.hpp
@@ -0,0 +1,53 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "std_image.h"
+
+
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <glad/glad.h>
+#include <iostream>
+
+class Texture
+{
+public:
+
+	Texture(const char* texturePath)
+	{
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		// set the texture wrapping parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// set texture filtering parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+		data = stbi_load(texturePath, &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else
+		{
+			std::cout << "Failed to load texture" << std::endl;
+		}
+		stbi_image_free(data);
+	}
+
+	void BindTexture()
+	{
+		glBindTexture(GL_TEXTURE_2D, texture);
+	}
+
+private:
+	unsigned int texture;
+	unsigned char* data;
+
+	int width, height, nrChannels;
+};
+
+#endif
---
+++ solarsimulation/vertexObject.hpp
@@ -0,0 +1,55 @@
+#ifndef VERTEXOBJECT_H
+#define VERTEXOBJECT_H
+#endif
+
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <glad/glad.h>
+#include <vector>
+
+class vObject
+{
+public:
+	vObject()
+	{
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	}
+
+	void arrtibArray()
+	{
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+	}
+
+	void vBindArray()
+	{
+		glBindVertexArray(VAO);
+	}
+
+	void deallocate()
+	{
+		glDeleteBuffers(1, &VBO);
+		glDeleteVertexArrays(1, &VAO);
+	}
+
+	glm::mat4 createModel(glm::mat4 model, glm::vec3 cubePosition, float rotation, float revolution)
+	{
+		model = glm::rotate(model, static_cast<float>(glfwGetTime()) * rotation, glm::vec3(1.0f, 0.3f, 0.5f));
+		model = glm::translate(model, cubePosition);
+		//model = glm::rotate(model, static_cast<float>(glfwGetTime()) / revolution, glm::vec3(1.0f, 0.3f, 0.5f));
+		return model;
+	}
+
+private:
+	unsigned int VAO;
+	unsigned int VBO;
+};
